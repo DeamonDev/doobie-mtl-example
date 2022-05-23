b16a6efad01f72f53533d1679f72fcbe3f0adbea
Piotr Rudnicki <deamondev@gmail.com>
2022-05-23 14:41:10 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 sudo su postgres <<EOF
-psql -c 'CREATE DATABASE manifolds;'
+psql -c 'CREATE DATABASE manifolds_atlas;'
 EOF
 
-su -c "psql -d manifolds -c \"CREATE TABLE IF NOT EXISTS algebraic_varieties (id INT PRIMARY KEY NOT NULL, name VARCHAR(50) UNIQUE NOT NULL,
+su -c "psql -d manifolds_atlas -c \"CREATE TABLE IF NOT EXISTS algebraic_varieties (id INT PRIMARY KEY NOT NULL, name VARCHAR(50) UNIQUE NOT NULL,
 equation VARCHAR(100) UNIQUE NOT NULL, euler_char INT); SELECT * FROM algebraic_varieties;\"" postgres
